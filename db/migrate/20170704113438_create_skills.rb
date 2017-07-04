331cb4ac2754adda36e535d319d258c6327eef99
christophe <christopheelkhoury@gmail.com>
2017-07-04 15:12:51 +0300

    Skills added

--- db/migrate/20170704113438_create_skills.rb
+++ db/migrate/20170704113438_create_skills.rb
@@ -1,7 +1,7 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
-      t.sting :title
+      t.string :title
       t.integer :percent_utilized
 
       t.timestamps

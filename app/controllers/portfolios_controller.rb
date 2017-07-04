4e3c297cf3544ffc0015d6a2284dff7b22da459c
christophe <christopheelkhoury@gmail.com>
2017-07-04 17:20:24 +0300

    Integrated thumbnail images into portfolio pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,5 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all # Call the model whch is inside the controller and make it available to the view
+  end
 end

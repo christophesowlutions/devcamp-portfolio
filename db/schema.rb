2dd419693a88ae4adf95b266fab269c212fb6f00
christophe <christopheelkhoury@gmail.com>
2017-07-05 13:13:44 +0300

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705094117) do
+ActiveRecord::Schema.define(version: 20170705100805) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170705094117) do
     t.datetime "updated_at", null: false
     t.string "author_name"
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 

c92e3e289b904c0ffe9a4c70418dde81f839b2de
christophe <christopheelkhoury@gmail.com>
2017-07-05 17:26:36 +0300

    68 implementing concerns

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,8 +7,17 @@
 #  percent_utilized :integer
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
+#  badge            :text
 #
 
 class Skill < ApplicationRecord
+  includes Placeholder
   validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
+  end
+
 end

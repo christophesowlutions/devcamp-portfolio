389b9eb311601ed22a9b2e09aa350a1eb1f24779
christophe <christopheelkhoury@gmail.com>
2017-07-06 13:00:13 +0300

    86 session tracker for dynamic source links (twitter w heik)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q] # Set q if and only if params exists
+  end
 end

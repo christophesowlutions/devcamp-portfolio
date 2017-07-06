d9d4832ba8536a0b702ec205a6a136b96761f364
christophe <christopheelkhoury@gmail.com>
2017-07-06 17:27:06 +0300

    98 implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def login_helper
+    if current_user.is_a?(User)
+      link_to "Logout", destroy_new_user_session_path, method: :delte
+    else
+      (link_to "Register", new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to "Login", new_user_session_path)
+    end
+  end
+
+  def source_helper
+    # Definition could be as follows:
+    # def source_helper (layout_name) and write if(layout_name = x) pass a design and if layout_name = y then pass another thing.
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end

1b53a5e4cd768c024be59e99c98ea3383621c68a
christophe <christopheelkhoury@gmail.com>
2017-07-10 16:55:59 +0300

    152 implemented persistent position updates for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # This created all kinds of routes for registration, log in, log out, etc.
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'log_out', sign_up: 'register' }
   # The above changes the following path names to the respective ones (sign_in to login ,etc.) 
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show' # Get the route with /:id and map it to the #show action as portoflio_show
   #, except: [:show] # Bring all of the resource routes except show, so I cna create my own show action
   #get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show' # because otherwise it would still submit it as portfolios

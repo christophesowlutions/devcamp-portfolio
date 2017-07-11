937874f828266c2f25ab56934664de34809ef3aa
christophe <christopheelkhoury@gmail.com>
2017-07-11 11:46:19 +0300

    disregard this commit

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -20,15 +20,13 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
-  after_initialize :set_defaults
+  #mount_uploader :thumb_image, PortfolioUploader
+  #mount_uploader :main_image, PortfolioUploader
 
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 
   def self.by_position
     order("position ASC")
   end
-end
 
+  scope :ruby_on_rails_portfolio_iems, -> { where(subtitle: 'Ruby on Rails') }
+end

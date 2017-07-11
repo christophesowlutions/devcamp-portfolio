7c92b8e5c4a455d481c70b640fece6008db47bea
christophe <christopheelkhoury@gmail.com>
2017-07-11 11:58:32 +0300

    166 added cocoon and JS to dynamically add new technology + Edit button for portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -15,8 +15,9 @@
 
 class Portfolio < ApplicationRecord
   has_many :technologies
-  accepts_nested_attributes_for :technologies, 
-                                reject_if: lambda { |attrs| attrs['name'].blank? }
+  accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,  # Makes destroying a technology possible
+                                reject_if: lambda { |attrs| attrs['name'].blank? } # Makes it impossible to add an empty "name" field
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
